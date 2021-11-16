4010df6097416cc50ded8bede7501de08ccc4584
Brian McGowan <brian.mcgowan@oreskos.com>
2021-11-16 13:37:17 -0500

    Add image rendering service

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,13 +1,16 @@
 import { CsvParserService } from './csv-parser.service';
+import { ImageRenderService } from './image-render.service';
 import { PointService } from './point.service';
 import { SpaceDataService } from './space-data.service';
 
 export const services: any[] = [
   CsvParserService,
+  ImageRenderService,
   PointService,
   SpaceDataService
 ];
 
-export * from './csv-parser.service'
+export * from './csv-parser.service';
+export * from './image-render.service';
 export * from './point.service';
 export * from './space-data.service';
